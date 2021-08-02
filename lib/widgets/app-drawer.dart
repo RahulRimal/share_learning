036d21a2c8538c23739ef97f1e35f0415ea39617
Rahul Rimal <genuinetechnology9@gmail.com>
2021-08-02 15:31:58 +0545

    Working on app drawer

--- lib/widgets/app-drawer.dart
+++ lib/widgets/app-drawer.dart
@@ -11,11 +11,33 @@ class AppDrawer extends StatelessWidget {
             tileColor: Theme.of(context).primaryColor,
             leading: Text(
               'Rahul Rimal',
-              style: TextStyle(color: Colors.white),
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
             trailing: CircleAvatar(
               backgroundImage: NetworkImage(
                   'https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255_960_720.jpg'),
+            ),
+          ),
+          ListTile(
+            tileColor: Theme.of(context).accentColor,
+            title: Text(
+              'Create new Post',
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          ListTile(
+            leading: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.bookmark),
+                Text('Wishlist'),
+              ],
             ),
           ),
         ],
