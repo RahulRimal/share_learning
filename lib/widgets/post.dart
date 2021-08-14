4b8aae734a4bcfcd120eb71f382815a5cf2d71bd
Rahul Rimal <genuinetechnology9@gmail.com>
2021-08-15 01:13:33 +0545

    Post widget almost complete..

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -6,23 +6,144 @@ class Post extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      padding: EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 30,
+      ),
       child: Stack(
-        overflow: Overflow.visible,
+        clipBehavior: Clip.none,
         children: [
           Container(
-            height: 200,
-            margin: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-            padding: EdgeInsets.all(10),
+            height: 240,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
             ),
-          ),
-          ListTile(
-            tileColor: Colors.white,
-            title: Text('C Programming Fundamentals II Edition'),
-            subtitle: Text(
-                'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
+            child: ListTile(
+              // tileColor: Colors.white,
+              contentPadding: EdgeInsets.only(
+                top: 17,
+                bottom: 50,
+                left: 10,
+                right: 10,
+              ),
+              title: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          'C Programming Fundamentals II Edition',
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Available',
+                        style: TextStyle(
+                          color: Colors.green[800],
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(Icons.person),
+                            Text('Author'),
+                            Flexible(
+                              child: Text(
+                                'Rahul Rimal',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(Icons.history),
+                            Flexible(child: Text('Bought time')),
+                            Flexible(
+                              child: Text(
+                                '1 Year ago',
+                                style: TextStyle(
+                                  color: Colors.yellow[300],
+                                  fontWeight: FontWeight.w600,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(Icons.monetization_on),
+                            Text('Price'),
+                            Text(
+                              'Rs. 600',
+                              style: TextStyle(
+                                color: Colors.green[800],
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.shop,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            Flexible(
+                              child: Text(
+                                'Add to Wishlist',
+                                style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                ],
+              ),
+              subtitle: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
           ),
           Positioned(
             top: -10,
