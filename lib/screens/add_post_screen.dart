2af0f9391d7577f352b793331b4e89f35d2ad98b
Rahul Rimal <genuinetechnology9@gmail.com>
2021-10-12 11:37:43 +0545

    Working on Add new post page

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:nepali_date_picker/nepali_date_picker.dart' as picker;
+import 'package:share_learning/models/book.dart';
 
 class AddPostScreen extends StatefulWidget {
   static const routeName = '/add-post';
@@ -10,23 +11,45 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
-  // final _form = GlobalKey<FormState>();
+  final _form = GlobalKey<FormState>();
+
+  final _authorFocusNode = FocusNode();
+  final _dateFocusNode = FocusNode();
+  final _priceFocusNode = FocusNode();
+  final _booksCountFocusNode = FocusNode();
+  final _descFocusNode = FocusNode();
+
+  picker.NepaliDateTime? _boughtTime;
+
+  // Book _edittedBook = new Book(
+  //   id: '',
+  //   author: '',
+  //   boughtTime: DateTime.now(),
+  //   description: '',
+  //   isWishlisted: false,
+  //   price: 0,
+  // );
 
   final _datePickercontroller = TextEditingController(
-    text: DateFormat('yyyy/MM/dd').format(picker.NepaliDateTime.now()).toString(),
-    
-    
+    text:
+        DateFormat('yyyy/MM/dd').format(picker.NepaliDateTime.now()).toString(),
   );
 
   Future<void> _showPicker(BuildContext context) async {
-    // await showDatePicker(
-        await picker.showAdaptiveDatePicker(
-        context: context,
-        initialDate: picker.NepaliDateTime.now(),
-        firstDate: picker.NepaliDateTime(2070),
-        lastDate: picker.NepaliDateTime.now(),
-        
-        );
+    _boughtTime = await picker.showAdaptiveDatePicker(
+      context: context,
+      initialDate: picker.NepaliDateTime.now(),
+      firstDate: picker.NepaliDateTime(2070),
+      lastDate: picker.NepaliDateTime.now(),
+    ) as picker.NepaliDateTime;
+
+    _datePickercontroller.text =
+        DateFormat('yyyy/MM/dd').format(_boughtTime as DateTime).toString();
+  }
+
+  void _savePost()
+  {
+    _form.currentState!.save();
   }
 
   @override
@@ -37,7 +60,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.save),
-            onPressed: (){},
+            onPressed: () {},
           ),
         ],
       ),
@@ -51,7 +74,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
             color: Colors.white,
           ),
           child: Form(
-            // key: _form,
+            key: _form,
             child: ListView(
               children: [
                 TextFormField(
@@ -66,9 +89,9 @@ class _AddPostScreenState extends State<AddPostScreen> {
                     ),
                   ),
                   textInputAction: TextInputAction.next,
-                  // onFieldSubmitted: (_) {
-                  //   FocusScope.of(context).requestFocus(_priceFocusNode);
-                  // },
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_authorFocusNode);
+                  },
                   validator: (value) {
                     if (value!.isEmpty) {
                       return 'Please provide a value.';
@@ -92,12 +115,16 @@ class _AddPostScreenState extends State<AddPostScreen> {
                         padding: const EdgeInsets.all(8.0),
                         child: TextFormField(
                           cursorColor: Theme.of(context).primaryColor,
+                          focusNode: _authorFocusNode,
                           decoration: InputDecoration(
                             labelText: 'Author',
                             focusColor: Colors.redAccent,
                           ),
                           textInputAction: TextInputAction.next,
                           autovalidateMode: AutovalidateMode.always,
+                          onFieldSubmitted: (_) {
+                            FocusScope.of(context).requestFocus(_dateFocusNode);
+                          },
                         ),
                       ),
                     ),
@@ -106,6 +133,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
                         padding: const EdgeInsets.all(8.0),
                         child: TextFormField(
                           controller: _datePickercontroller,
+                          focusNode: _dateFocusNode,
                           // initialValue:
                           //     DateFormat('yyyy/MM/dd').format(DateTime.now()),
                           keyboardType: TextInputType.datetime,
@@ -129,11 +157,17 @@ class _AddPostScreenState extends State<AddPostScreen> {
                               tooltip: 'Tap to open date picker',
                               onPressed: () {
                                 _showPicker(context);
+
+                                // _datePickercontroller.text = _boughtTime.toString();
                               },
                             ),
                           ),
                           textInputAction: TextInputAction.next,
                           autovalidateMode: AutovalidateMode.always,
+                          onFieldSubmitted: (_) {
+                            FocusScope.of(context)
+                                .requestFocus(_priceFocusNode);
+                          },
                         ),
                       ),
                     ),
@@ -146,6 +180,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
                         padding: const EdgeInsets.all(8.0),
                         child: TextFormField(
                           cursorColor: Theme.of(context).primaryColor,
+                          focusNode: _priceFocusNode,
                           decoration: InputDecoration(
                             prefix: Text('Rs. '),
                             labelText: 'Price',
@@ -153,6 +188,10 @@ class _AddPostScreenState extends State<AddPostScreen> {
                           ),
                           textInputAction: TextInputAction.next,
                           autovalidateMode: AutovalidateMode.always,
+                          onFieldSubmitted: (_) {
+                            FocusScope.of(context)
+                                .requestFocus(_booksCountFocusNode);
+                          },
                         ),
                       ),
                     ),
@@ -160,6 +199,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: TextFormField(
+                          focusNode: _booksCountFocusNode,
                           initialValue: '1',
                           keyboardType: TextInputType.number,
                           cursorColor: Theme.of(context).primaryColor,
@@ -168,11 +208,49 @@ class _AddPostScreenState extends State<AddPostScreen> {
                           ),
                           textInputAction: TextInputAction.next,
                           autovalidateMode: AutovalidateMode.always,
+                          onFieldSubmitted: (_) {
+                            FocusScope.of(context).requestFocus(_descFocusNode);
+                          },
                         ),
                       ),
                     ),
                   ],
                 ),
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      focusNode: _descFocusNode,
+                      keyboardType: TextInputType.number,
+                      cursorColor: Theme.of(context).primaryColor,
+                      decoration: InputDecoration(
+                        labelText: 'Book description',
+                      ),
+                      textInputAction: TextInputAction.newline,
+                      autovalidateMode: AutovalidateMode.always,
+                      // onFieldSubmitted: (_) {
+                      //   FocusScope.of(context).requestFocus(_descFocusNode);
+                      // },
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          Theme.of(context).primaryColor),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Add this Post',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
           ),
