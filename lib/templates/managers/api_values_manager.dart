8b28c1933148b754137de8288156b6499b26b447
Rahul Rimal <genuinetechnology9@gmail.com>
2022-06-06 10:00:08 +0545

    Working on User Profile Screen

--- lib/templates/managers/api_values_manager.dart
+++ lib/templates/managers/api_values_manager.dart
@@ -1,14 +1,14 @@
 class RemoteManager {
-  // static const String BASE_URI = 'http://10.0.2.2/apiforsharelearn';
-  static const String BASE_URI = 'https://ojasfilms.org/apiforshareLearn';
+  static const String BASE_URI = 'http://10.0.2.2/apiforsharelearn';
+  // static const String BASE_URI = 'https://ojasfilms.org/apiforshareLearn';
 
   static const String PROFILE_POOL =
-      // 'http://10.0.2.2/apiforsharelearn/images/profile-pics';
-      'https://ojasfilms.org/apiforshareLearn/images/profile-pics';
+      'http://10.0.2.2/apiforsharelearn/images/profile-pics';
+  // 'https://ojasfilms.org/apiforshareLearn/images/profile-pics';
 
   static const String POST_POOL =
-      // 'http://10.0.2.2/apiforsharelearn/images/posts';
-      'https://ojasfilms.org/apiforshareLearn/images/posts';
+      'http://10.0.2.2/apiforsharelearn/images/posts';
+  // 'https://ojasfilms.org/apiforshareLearn/images/posts';
 }
 
 class ApiManager {}
--- lib/templates/screens/user_profile_screen.dart
+++ lib/templates/screens/user_profile_screen.dart
@@ -12,7 +12,9 @@ import 'package:share_learning/templates/screens/user_profile_edit_screen.dart';
 import 'package:share_learning/templates/utils/user_helper.dart';
 
 class UserProfileScreen extends StatefulWidget {
-  const UserProfileScreen({Key? key}) : super(key: key);
+  const UserProfileScreen({
+    Key? key,
+  }) : super(key: key);
 
   static const String routeName = '/user-profile';
 
@@ -46,89 +48,324 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
 
     return Container(
       padding: EdgeInsets.only(top: AppPadding.p12),
-      child: Column(
-        children: [
-          ListTile(
-            horizontalTitleGap: AppHeight.h8,
-            contentPadding: EdgeInsets.zero,
-            leading: CircleAvatar(
-              // radius: AppPadding.p16,
-              // radius: AppRadius.r100,
-              radius: 70,
-              backgroundImage: user.image != null
-                  ? NetworkImage(
-                      UserHelper.userProfileImage(user),
-                    )
-                  : Image.asset(
-                      ImageAssets.noProfile,
-                    ) as ImageProvider,
-              // foregroundImage: NetworkImage(
-              //   UserHelper.userProfileImage(user),
-              // ),
-            ),
-            // title: Text(user.firstName),
-            title: Text(
-              UserHelper.userDisplayName(user),
-              textAlign: TextAlign.center,
-              style: getBoldStyle(
-                fontSize: FontSize.s20,
-                color: ColorManager.black,
+      // child: Column(
+      //   children: [
+      //     ListTile(
+      //       horizontalTitleGap: AppHeight.h8,
+      //       contentPadding: EdgeInsets.zero,
+      //       leading: CircleAvatar(
+      //         // radius: AppPadding.p16,
+      //         // radius: AppRadius.r100,
+      //         radius: 70,
+      //         backgroundImage: user.image != null
+      //             ? NetworkImage(
+      //                 UserHelper.userProfileImage(user),
+      //               )
+      //             : Image.asset(
+      //                 ImageAssets.noProfile,
+      //               ) as ImageProvider,
+      //         // foregroundImage: NetworkImage(
+      //         //   UserHelper.userProfileImage(user),
+      //         // ),
+      //       ),
+      //       // title: Text(user.firstName),
+      //       title: Text(
+      //         UserHelper.userDisplayName(user),
+      //         textAlign: TextAlign.center,
+      //         style: getBoldStyle(
+      //           fontSize: FontSize.s20,
+      //           color: ColorManager.black,
+      //         ),
+      //       ),
+      //       subtitle: Text(
+      //         user.description.toString(),
+      //         // 'kfdjlfdkflsd djfkjfioa kd fdojfka kdfklafj jklkkljljklkk ll',
+      //         // textAlign: TextAlign.justify,
+      //         overflow: TextOverflow.ellipsis,
+      //         maxLines: 2,
+      //         style: getRegularStyle(
+      //           fontSize: FontSize.s12,
+      //           color: ColorManager.black,
+      //         ),
+      //       ),
+      //     ),
+      //     // Container(
+      //     //   // height: AppHeight.h100,
+      //     //   child: Row(
+      //     //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //     //     children: [
+      //     //       CircleAvatar(
+      //     //         radius: AppRadius.r50,
+      //     //         backgroundImage: NetworkImage(
+      //     //           UserHelper.userProfileImage(user),
+      //     //         ),
+      //     //       ),
+      //     //       // Text(user.firstName + user.lastName),
+      //     //       Column(
+      //     //         children: [
+      //     //           Text(
+      //     //             UserHelper.userDisplayName(user),
+      //     //             style: getBoldStyle(
+      //     //               fontSize: FontSize.s20,
+      //     //               color: ColorManager.black,
+      //     //             ),
+      //     //           ),
+      //     //           Text(
+      //     //             // user.description.toString(),
+      //     //             'kfdjlfdkflsd djfkjfioa kd fdojfka kdfklafj jklkkljljklkk llllllll',
+      //     //             textAlign: TextAlign.left,
+      //     //             overflow: TextOverflow.ellipsis,
+      //     //             maxLines: 2,
+      //     //             style: getRegularStyle(
+      //     //               fontSize: FontSize.s12,
+      //     //               color: ColorManager.black,
+      //     //             ),
+      //     //           ),
+      //     //         ],
+      //     //       ),
+      //     //     ],
+      //     //   ),
+      //     // ),
+      //     // Text(userSessionData!.accessToken),
+      //     // Text(userSessionData.accessTokenExpiry.toString()),
+      //   ],
+      // ),
+      child: Column(children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 360,
+              decoration: BoxDecoration(
+                // color: ColorManager.darkPrimary,
+                color: ColorManager.white,
+              ),
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 60, 0, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Card(
+                            clipBehavior: Clip.antiAliasWithSaveLayer,
+                            color: ColorManager.primary,
+                            elevation: 2,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40),
+                            ),
+                            child: Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(2, 2, 2, 2),
+                              child: Container(
+                                width: 80,
+                                height: 80,
+                                clipBehavior: Clip.antiAlias,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Image.asset(
+                                  ImageAssets.noProfile,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(0, 0, 16, 0),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Container(
+                                  width: 44,
+                                  height: 44,
+                                  decoration: BoxDecoration(
+                                    color: ColorManager.primary,
+                                    borderRadius: BorderRadius.circular(8),
+                                    border: Border.all(
+                                      color: ColorManager.primary,
+                                      width: 2,
+                                    ),
+                                  ),
+                                  child: IconButton(
+                                    icon: Icon(
+                                      Icons.edit_outlined,
+                                      color: ColorManager.white,
+                                      size: AppSize.s24,
+                                    ),
+                                    onPressed: () {
+                                      Navigator.of(context).pushNamed(
+                                        UserProfileEditScreen.routeName,
+                                        arguments: {
+                                          // 'loggedInUserSession': userSession.session,
+                                          'loggedInUserSession': userSession,
+                                          'user': user,
+                                        },
+                                      );
+                                    },
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      12, 0, 0, 0),
+                                  child: Container(
+                                    width: 44,
+                                    height: 44,
+                                    decoration: BoxDecoration(
+                                      color: ColorManager.darkSecondary,
+                                      borderRadius: BorderRadius.circular(8),
+                                      border: Border.all(
+                                        color: ColorManager.primary,
+                                        width: 2,
+                                      ),
+                                    ),
+                                    child: IconButton(
+                                      icon: Icon(
+                                        Icons.login_rounded,
+                                        color: ColorManager.secondary,
+                                        size: 24,
+                                      ),
+                                      onPressed: () async {
+                                        // await signOut();
+                                        // await Navigator
+                                        //     .pushAndRemoveUntil(
+                                        //   context,
+                                        //   MaterialPageRoute(
+                                        //     builder: (context) =>
+                                        //         LoginPageWidget(),
+                                        //   ),
+                                        // );
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Text(
+                            // 'Rahul Rimal',
+                            UserHelper.userDisplayName(user),
+                            style: getBoldStyle(
+                                fontSize: FontSize.s12,
+                                color: ColorManager.primary),
+                          ),
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(4, 0, 0, 0),
+                            child: Text(
+                              // profilePageUsersRecord.age.toString(),
+                              "89",
+                              style: getBoldStyle(
+                                color: ColorManager.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                          child: Text(
+                              // profilePageUsersRecord.email,
+                              'mail@mail.com',
+                              style:
+                                  getRegularStyle(color: ColorManager.primary)),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 12, 20, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Text('Ailments',
+                              textAlign: TextAlign.start,
+                              style: getBoldStyle(color: ColorManager.primary)),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 8, 20, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Expanded(
+                            child: Container(),
+                            // child: AutoSizeText(
+                            //   profilePageUsersRecord.ailments,
+                            //   style:
+                            //       FlutterFlowTheme.of(context).title3,
+                            // ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 12),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Column(
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Next Appointment',
+                                  textAlign: TextAlign.start,
+                                  style: getRegularStyle(
+                                      color: ColorManager.primary)),
+                              Padding(
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                                child: Text(
+                                  'Aug 20, 2021',
+                                  style: getRegularStyle(
+                                    color: ColorManager.primary,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            subtitle: Text(
-              user.description.toString(),
-              // 'kfdjlfdkflsd djfkjfioa kd fdojfka kdfklafj jklkkljljklkk ll',
-              // textAlign: TextAlign.justify,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 2,
-              style: getRegularStyle(
-                fontSize: FontSize.s12,
-                color: ColorManager.black,
-              ),
+          ],
+        ),
+        Column(mainAxisSize: MainAxisSize.max, children: [
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(20, 12, 20, 12),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Text(
+                  'Past Appointments',
+                  style: getBoldStyle(
+                    color: ColorManager.primary,
+                  ),
+                ),
+              ],
             ),
           ),
-
-          // Container(
-          //   // height: AppHeight.h100,
-          //   child: Row(
-          //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-          //     children: [
-          //       CircleAvatar(
-          //         radius: AppRadius.r50,
-          //         backgroundImage: NetworkImage(
-          //           UserHelper.userProfileImage(user),
-          //         ),
-          //       ),
-          //       // Text(user.firstName + user.lastName),
-          //       Column(
-          //         children: [
-          //           Text(
-          //             UserHelper.userDisplayName(user),
-          //             style: getBoldStyle(
-          //               fontSize: FontSize.s20,
-          //               color: ColorManager.black,
-          //             ),
-          //           ),
-          //           Text(
-          //             // user.description.toString(),
-          //             'kfdjlfdkflsd djfkjfioa kd fdojfka kdfklafj jklkkljljklkk llllllll',
-          //             textAlign: TextAlign.left,
-          //             overflow: TextOverflow.ellipsis,
-          //             maxLines: 2,
-          //             style: getRegularStyle(
-          //               fontSize: FontSize.s12,
-          //               color: ColorManager.black,
-          //             ),
-          //           ),
-          //         ],
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          // Text(userSessionData!.accessToken),
-          // Text(userSessionData.accessTokenExpiry.toString()),
-        ],
-      ),
+        ]),
+      ]),
     );
   }
 
